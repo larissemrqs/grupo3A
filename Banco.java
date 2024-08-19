4339528a9b81e4b88d5a17421f3d71b77d52f6d2
grupo1 <lmp3@aluno.ifal.edu.br>
2024-08-19 12:15:39 -0300

    inicio da classe Banco

---
+++ Banco.java
@@ -0,0 +1,14 @@
+public class Banco {
+
+String nome = "BancoDV";
+String contas = "";
+
+String titular;
+double saldo_inicial;
+
+public void criar_conta(String titular, double saldo_inicial) {
+
+}
+
+}
+
